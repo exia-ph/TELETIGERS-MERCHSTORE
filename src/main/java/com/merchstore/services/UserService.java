3fecb646343a408a83a6fbc3003c44071a4b222d
Jude Rabang <juderabang@gmail.com>
2022-02-20 00:25:01 +0800

    added User Service

---
+++ src/main/java/com/merchstore/services/UserService.java
@@ -0,0 +1,15 @@
+package com.merchstore.services;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    //methods to be done with the users (just placeholder methods below)
+    public List<User> getUser() {
+        return List.of(
+                new User("jude","123","admin","j@gmail.com","v,pang")   //replace with database code, just a placeholder
+        );
+    }
+}
